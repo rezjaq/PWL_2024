d5977ab89dc15ef5a362067f27fd3a1998801017
Rizqi Reza Danuarta <ejaaa646@gmail.com>
2024-02-21 07:12:06 +0700

    update

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    public function about()
+    {
+        return 'Nama : Rizqi Reza Danuarta<br>NIM : 2241720057';
+    }
+}
